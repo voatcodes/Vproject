3dff2751ac95e444f791ab2e6e31280d11d095b1
voatcodes <valarietila@gmail.com>
2022-10-25 11:04:31 +0300

    declared routes for users and businesses

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var businessesRouter = require('./routes/businesses');
+
 
 
 
@@ -29,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/businesses', businessesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
