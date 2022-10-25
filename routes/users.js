be39ccc9ce811afd0bf697ab4c9566dcef40cc02
voatcodes <valarietila@gmail.com>
2022-10-25 11:06:39 +0300

    routed signup and login views

--- routes/users.js
+++ routes/users.js
@@ -18,17 +18,17 @@ router.use(session({
 
 
 //continously check if user is logged in
-router.use((req, res, next) => {
-  if (req.session.userID === undefined) {
-      res.locals.isLoggedIn = false
-  } else {
-      res.locals.isLoggedIn = true
-      res.locals.username = req.session.username
-      res.locals.user = req.session.user
-      res.locals.business = req.session.profile
-  }
-  next()
-})
+// router.use((req, res, next) => {
+//   if (req.session.userID === undefined) {
+//     res.locals.isLoggedIn = false
+//   } else {
+//     res.locals.isLoggedIn = true
+//     res.locals.username = req.session.username
+//     res.locals.user = req.session.user
+//     res.locals.business = req.session.profile
+//   }
+//   next()
+// })
 
 
 
@@ -42,11 +42,11 @@ module.exports = router;
 // display login form
 
 router.get('/login', (req, res) => {
-  const user ={
-    email:'',
-    password:'',
+  const user = {
+    email: '',
+    password: '',
   }
-  res.render('users/login', {error:false, user:user})
+  res.render('users/login', { error: false, user: user })
 })
 
 
@@ -56,35 +56,35 @@ router.get('/login', (req, res) => {
 router.post('/login', (req, res) => {
 
   connection.query(
-      'SELECT * FROM user_profile WHERE email = ?',
-      [req.body.email],
-      (error, results) => {
-          if (results.length > 0) {
-//               // authenticate 
-              bcrypt.compare(req.body.password, results[0].password, (error, matches) => {
-                  if (matches) {
-                      req.session.userID = results[0].userID
-                      req.session.username = results[0].fullname.split(' ')[0]
-                      req.session.user = 'reviewer'
-                      res.redirect('/app')
-                  } else {
-                      const user = {
-                          email: req.body.email,
-                          password: req.body.password
-                      }
-                      let message = 'Email/Password mismatch.'
-                      res.render('reviewerprofile', { error: true, message: message, user: user })
-                  }
-              })
+    'SELECT * FROM user_profile WHERE email = ?',
+    [req.body.email],
+    (error, results) => {
+      if (results.length > 0) {
+        //               // authenticate 
+        bcrypt.compare(req.body.password, results[0].pword, (error, matches) => {
+          if (matches) {
+            req.session.userID = results[0].userID
+            req.session.username = results[0].fullname.split(' ')[0]
+            req.session.user = 'reviewer'
+            res.redirect('/app')
           } else {
-              const user = {
-                  email: req.body.email,
-                  password: req.body.password
-              }
-              let message = 'Account does not exist. Please create one.'
-              res.render('users/signup', { error: true, message: message, user: user })
+            const user = {
+              email: req.body.email,
+              password: req.body.password
+            }
+            let message = 'Email/Password mismatch.'
+            res.render('reviewerprofile', { error: true, message: message, user: user })
           }
+        })
+      } else {
+        const user = {
+          email: req.body.email,
+          password: req.body.password
+        }
+        let message = 'Account does not exist. Please create one.'
+        res.render('users/signup', { error: true, message: message, user: user })
       }
+    }
   )
 })
 
@@ -112,68 +112,64 @@ router.get('/signup', (req, res) => {
 
 
 
- //submit signup form
+//submit signup form
 
 router.post('/signup', (req, res) => {
 
+  const user = {
+    dob: req.body.dob,
+    fullname: req.body.fullname,
+    preferedUsername: req.body.preferedUsername,
+    email: req.body.email,
+    pNumber: req.body.pNumber,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword
 
-  if(req.body.password === req.body.confirmPassword) {
+  }
+
+
+  if (req.body.password === req.body.confirmPassword) {
 
     connection.query(
 
       'SELECT email FROM user_profile WHERE email = ?',
       [req.body.email],
-      (error,results) => {
-        if(results.length > 0){
+      (error, results) => {
+        if (results.length > 0) {
 
-          const user = {
-            dob: req.body.dob,
-            fullname: req.body.fullname,
-            preferedUsername: req.body.preferedUsername,
-            email: req.body.email,
-            pNumber: req.body.pNumber,
-            password: req.body.password,
-            confirmPassword: req.body.confirmPassword
+          
 
-        }
+          let message = 'Account with this email already exists'
+          res.render('users/login', { error: true, message: message, user: user })
 
-        let message = 'Account with this email already exists'
-        res.render('users/login', {error: true, message:message, user: user})
-           
-      
-        } else{
-          bcrypt.hash(req.body.password, 10, (error, hash) => {
+
+        } else {
+          bcrypt.hash(user.password, 10, (error, hash) => {
+            user.password = hash
             connection.query(
               'INSERT INTO user_profile (dob, fullname, preferedusername, email, pnumber, pword) VALUES (?,?,?,?,?,?)',
-              
-              [req.body.dob, req.body.fullname, req.body.preferedUsername, req.body.email, req.body.pNumber, hash],
-          
+
+              [user.dob, user.fullname, user.preferedUsername, user.email, user.pNumber, hash],
+
               (error, results) => {
+                // console.log(user)
+                console.log(results)
                 res.redirect('/users/login')
               }
             )
           })
         }
-        }
-        )
-       } else {
+      }
+    )
+  } else {
 
-        const user = {
-                      dob: req.body.dob,
-                      fullname: req.body.fullname,
-                      preferedUsername: req.body.preferedUsername,
-                      email: req.body.email,
-                      pNumber: req.body.pNumber,
-                      password: req.body.password,
-                      confirmPassword: req.body.confirmPassword
-          
-                  }
+    
 
-      let message ='Password and confirm password does not match!'
-      res.render('/users/signup', {error:true, message:message, user:user})
+    let message = 'Password and confirm password does not match!'
+    res.render('/users/signup', { error: true, message: message, user: user })
 
-        }
-      })
+  }
+})
 
 
 
@@ -193,7 +189,7 @@ router.get('/reviewerprofile/:id', (req, res) => {
     instagramURL: '',
     photoURL: ''
   }
-  res.render('/reviewerprofile', { error: false, profile: profile })
+  res.render('reviewerprofile', { error: false, profile: profile })
 })
 
 
@@ -201,110 +197,110 @@ router.get('/reviewerprofile/:id', (req, res) => {
 //display reviewers edit profile form
 
 router.get('/editprofile/:id', (req, res) => {
-  
 
-      let sql = 'SELECT * FROM  user_profile WHERE userID =?'
 
-      connection.query(
-          sql, [parseInt(req.params.id)],
-          (error, results) => {
-            
-              const profile = {
-                  userID: results[0].userID,
-                  dob: results[0].dob,
-                  fullName:results[0].fullname,
-                  userName: results[0].preferedusername,
-                  email: results[0].email,
-                  phoneNumber: results[0].pnumber,
-                  facebookURL: results[0].facebookURL,
-                  twitterURL: results[0].twitterURL,
-                  instagramURL: results[0].instagramURL,
-                  dateJoined: results[0].datejoined,
-                  photoURL: results[0].photoURL,
-                  cphotoURL: results[0].photoURL
-              }
-                console.log(profile)
+  let sql = 'SELECT * FROM  user_profile WHERE userID =?'
 
-              res.render('reviewereditprofile', { profile: profile })
-              }
+  connection.query(
+    sql, [parseInt(req.params.id)],
+    (error, results) => {
 
-           )   
-              
-          })
+      const profile = {
+        userID: results[0].userID,
+        dob: results[0].dob,
+        fullName: results[0].fullname,
+        userName: results[0].preferedusername,
+        email: results[0].email,
+        phoneNumber: results[0].pnumber,
+        facebookURL: results[0].facebookURL,
+        twitterURL: results[0].twitterURL,
+        instagramURL: results[0].instagramURL,
+        dateJoined: results[0].datejoined,
+        photoURL: results[0].photoURL,
+        cphotoURL: results[0].photoURL
+      }
+      console.log(profile)
+
+      res.render('reviewereditprofile', { profile: profile })
+    }
+
+  )
+
+})
 
 
 //submit reviewer edit profile
 
 router.post('/editprofile/:id', upload.single('photoURL'), (req, res) => {
   const profile = {
-      dob: req.body.dob,
-      fullname: req.body.fullname,
-      username: req.body.username,
-      email: req.body.email,
-      phoneNumber: req.body.phoneNumber,
-      facebookURL: req.body.facebookURL,
-      twitterURL: req.body.twitterURL,
-      instagramURL: req.body.instagramURL,
-      dateJoined: req.body.datejoined,
-      cphotoURL: undefined,
-      photoURL: undefined
+    dob: req.body.dob,
+    fullname: req.body.fullname,
+    username: req.body.username,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    facebookURL: req.body.facebookURL,
+    twitterURL: req.body.twitterURL,
+    instagramURL: req.body.instagramURL,
+    dateJoined: req.body.datejoined,
+    cphotoURL: undefined,
+    photoURL: undefined
   }
 
   if (req.file) {
 
-      profile.photoURL = req.file.filename
-      
-      let sql = 'UPDATE user_profile  SET dob=?, fullname = ?, preferedusername=?, email = ?, pnumber = ?,  facebookURL = ?, twitterURL = ?, instagramURL = ?, photoURL = ?, cphotoURL = ?, datejoined=? WHERE userID = ?'
+    profile.photoURL = req.file.filename
+
+    let sql = 'UPDATE user_profile  SET dob=?, fullname = ?, preferedusername=?, email = ?, pnumber = ?,  facebookURL = ?, twitterURL = ?, instagramURL = ?, photoURL = ?, cphotoURL = ?, datejoined=? WHERE userID = ?'
 
 
-      connection.query(
-          sql,
-          [             
-                profile.dob, 
-                profile.fullname,
-                profile.username,
-                profile.email,
-                profile.phoneNumber,
-                profile.facebookURL,
-                profile.twitterURL,
-                profile.instagramURL,
-                profile.photoURL,
-                profile.cphotoURL,
-                profile.dateJoined
-      
-          ],
-          (error, results) => {
-              res.redirect('reviewerprofile')
-              console.log(profile)
-          }
-      )
+    connection.query(
+      sql,
+      [
+        profile.dob,
+        profile.fullname,
+        profile.username,
+        profile.email,
+        profile.phoneNumber,
+        profile.facebookURL,
+        profile.twitterURL,
+        profile.instagramURL,
+        profile.photoURL,
+        profile.cphotoURL,
+        profile.dateJoined
+
+      ],
+      (error, results) => {
+        res.redirect('reviewerprofile')
+        console.log(profile)
+      }
+    )
 
   } else {
-      let sql = 'UPDATE user_profile  SET dob=?, fullname = ?, preferedusername=?, email = ?, pnumber = ?,  facebookURL = ?, twitterURL = ?, instagramURL = ?, photoURL = ?, cphotoURL = ?, datejoined=? WHERE userID = ?'
+    let sql = 'UPDATE user_profile  SET dob=?, fullname = ?, preferedusername=?, email = ?, pnumber = ?,  facebookURL = ?, twitterURL = ?, instagramURL = ?, photoURL = ?, cphotoURL = ?, datejoined=? WHERE userID = ?'
 
-      connection.query(
-          sql,
-          [
-           profile.dob, 
-                profile.fullname,
-                profile.username,
-                profile.email,
-                profile.phoneNumber,
-                profile.facebookURL,
-                profile.twitterURL,
-                profile.instagramURL,
-                profile.photoURL,
-                profile.cphotoURL,
-                profile.dateJoined       
+    connection.query(
+      sql,
+      [
+        profile.dob,
+        profile.fullname,
+        profile.username,
+        profile.email,
+        profile.phoneNumber,
+        profile.facebookURL,
+        profile.twitterURL,
+        profile.instagramURL,
+        profile.photoURL,
+        profile.cphotoURL,
+        profile.dateJoined
 
 
-          ],
-          (error, results) => {
-              res.redirect('reviewerprofile')
-              console.log(profile)
-          }
-      )
-    
+      ],
+      (error, results) => {
+        res.redirect('reviewerprofile')
+        console.log(profile)
+      }
+    )
+
   }
 
 })
